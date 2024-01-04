9fce57f38f48367f737cda6f0fd9dc37ea74bede
AlenOl <stoveprofi@gmail.com>
2024-01-04 20:37:34 +0000

    Test project implemented

---
+++ items/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+from django.shortcuts import render
+from .models import Item
+
+def item_list(request):
+    items = Item.objects.all()
+    return render(request, 'item_list.html', {'items': items})
+
